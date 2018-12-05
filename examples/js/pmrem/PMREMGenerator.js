b11302c6547eea62581a1c42e862ba091de25861
Andrew Khosravian <akhosravian@lowesinno.onmicrosoft.com>
2018-12-05 15:16:34 -0800

    formatting & variable names

--- examples/js/pmrem/PMREMGenerator.js
+++ examples/js/pmrem/PMREMGenerator.js
@@ -9,10 +9,10 @@
  * Code in the beginning of fragment shader's main function does this job for a given resolution.
  */
 
-THREE.PMREMGenerator = function ( sourceTexture, samplesPerLevel, resolution, sourceResolution ) {
+THREE.PMREMGenerator = function ( sourceTexture, samplesPerLevel, resolution, envMapFaceSize ) {
 
 	this.sourceTexture = sourceTexture;
-  this.sourceResolution = ( sourceResolution !== undefined ) ? sourceResolution : 512;
+	this.envMapFaceSize = ( envMapFaceSize !== undefined ) ? envMapFaceSize : 512;
 	this.resolution = ( resolution !== undefined ) ? resolution : 256; // NODE: 256 is currently hard coded in the glsl code for performance reasons
 	this.samplesPerLevel = ( samplesPerLevel !== undefined ) ? samplesPerLevel : 32;
 
@@ -71,7 +71,7 @@ THREE.PMREMGenerator.prototype = {
 
 		this.shader.uniforms[ 'envMap' ].value = this.sourceTexture;
 		this.shader.envMap = this.sourceTexture;
-    this.shader.uniforms[ 'sourceSize' ].value = this.sourceResolution;
+		this.shader.uniforms[ 'envMapFaceSize' ].value = this.envMapFaceSize;
 
 		var gammaInput = renderer.gammaInput;
 		var gammaOutput = renderer.gammaOutput;
@@ -88,7 +88,7 @@ THREE.PMREMGenerator.prototype = {
 
 			var r = i / ( this.numLods - 1 );
 			this.shader.uniforms[ 'roughness' ].value = r;
-      this.shader.uniforms[ 'mapSize' ].value = this.cubeLods[ i ].width;
+			this.shader.uniforms[ 'mapSize' ].value = this.cubeLods[ i ].width;
 			this.renderToCubeMapTarget( renderer, this.cubeLods[ i ] );
 		}
 
@@ -130,9 +130,8 @@ THREE.PMREMGenerator.prototype = {
 				"faceIndex": { value: 0 },
 				"roughness": { value: 0.5 },
 				"mapSize": { value: 0.5 },
-        "sourceSize": { value : 256.0 },
+				"envMapFaceSize": { value : 256.0 },
 				"envMap": { value: null },
-				"testColor": { value: new THREE.Vector3( 1, 1, 1 ) },
 			},
 
 			vertexShader:
@@ -142,11 +141,11 @@ THREE.PMREMGenerator.prototype = {
 					gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n\
 				}",
 
-      /* Andrew Khosravian: Hammersley2d functionality from 
-       * https://learnopengl.com/PBR/IBL/Specular-IBL
-       * more info at 
-       * https://blog.selfshadow.com/publications/s2013-shading-course/karis/s2013_pbs_epic_notes_v2.pdf
-       */
+			/* Andrew Khosravian: Hammersley2d functionality from 
+			 * https://learnopengl.com/PBR/IBL/Specular-IBL
+			 * more info at 
+			 * https://blog.selfshadow.com/publications/s2013-shading-course/karis/s2013_pbs_epic_notes_v2.pdf
+			 */
 			fragmentShader:
 				"#include <common>\n\
 				varying vec2 vUv;\n\
@@ -154,8 +153,7 @@ THREE.PMREMGenerator.prototype = {
 				uniform float roughness;\n\
 				uniform samplerCube envMap;\n\
 				uniform float mapSize;\n\
-				uniform float sourceSize;\n\
-				uniform vec3 testColor;\n\
+				uniform float envMapFaceSize;\n\
 				mat3 MatrixFromVector(vec3 n) {\n\
 					float a = 1.0 / (1.0 + n.z);\n\
 					float b = -n.x * n.y * a;\n\
@@ -163,7 +161,7 @@ THREE.PMREMGenerator.prototype = {
 					vec3 b2 = vec3(b, 1.0 - n.y * n.y * a, -n.y);\n\
 					return mat3(b1, b2, n);\n\
 				}\n\
-        vec3 GetSampleDirection() {\n\
+				vec3 GetSampleDirection() {\n\
 					vec2 uv = vUv*2.0 - 1.0;\n\
 					float offset = -1.0/mapSize;\n\
 					const float a = -1.0;\n\
@@ -186,8 +184,8 @@ THREE.PMREMGenerator.prototype = {
 					} else {\n\
 						return vec3(uv.x, -uv.y, -1.0);\n\
 					}\n\
-        }\n\
-        /*\n\
+				}\n\
+				/*\n\
 				float RadicalInverse_VdC(uint bits) {\n\
 					bits = (bits << 16u) | (bits >> 16u);\n\
 					bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);\n\
@@ -196,21 +194,21 @@ THREE.PMREMGenerator.prototype = {
 					bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);\n\
 					return float(bits) * 2.3283064365386963e-10; // / 0x100000000\n\
 				}\n\
-        */\n\
-        // slow version using mod since glsl es 2.0 doesn't support bit operations\n\
-        float RadicalInverse_VdC(int bits) {\n\
-          float result = 0.0;\n\
-          float denom = 1.0;\n\
-          float invBase = 0.5;\n\
-          for (int i = 0; i < 32; ++i) {\n\
-            denom = mod(float(bits), 2.0);\n\
-            result += denom * invBase;\n\
-            invBase *= 0.5;\n\
-            bits = int(float(bits) * 0.5);\n\
-            if (bits == 0) break;\n\
-          }\n\
-          return result;\n\
-        }\n\
+				*/\n\
+				// slow version using mod since glsl es 2.0 doesn't support bit operations\n\
+				float RadicalInverse_VdC(int bits) {\n\
+					float result = 0.0;\n\
+					float denom = 1.0;\n\
+					float invBase = 0.5;\n\
+					for (int i = 0; i < 32; ++i) {\n\
+						denom = mod(float(bits), 2.0);\n\
+						result += denom * invBase;\n\
+						invBase *= 0.5;\n\
+						bits = int(float(bits) * 0.5);\n\
+						if (bits == 0) break;\n\
+					}\n\
+					return result;\n\
+				}\n\
 				vec2 Hammersley2d(/*uint*/int i, /*uint*/int N) {\n\
 					return vec2(float(i)/float(N), RadicalInverse_VdC(i));\n\
 				}\n\
@@ -222,11 +220,11 @@ THREE.PMREMGenerator.prototype = {
 					float SinTheta = sqrt( 1.0 - CosTheta * CosTheta );\n\
 					return vecSpace * vec3(SinTheta * cos( Phi ), SinTheta * sin( Phi ), CosTheta);\n\
 				}\n\
-        float DistributionGGX(float NdotH) {\n\
-          float rSq = roughness * roughness;\n\
-          float NdotHSq = NdotH * NdotH;\n\
-          return rSq / pow(NdotHSq * (rSq - 1.0) + 1.0, 2.0);\n\
-        }\n\
+				float DistributionGGX(float NdotH) {\n\
+					float rSq = roughness * roughness;\n\
+					float NdotHSq = NdotH * NdotH;\n\
+					return rSq / pow(NdotHSq * (rSq - 1.0) + 1.0, 2.0);\n\
+				}\n\
 				vec3 testColorMap() {\n\
 					vec3 color;\n\
 					if(faceIndex == 0)\n\
@@ -246,32 +244,32 @@ THREE.PMREMGenerator.prototype = {
 				}\n\
 				void main() {\n\
 					vec3 sampleDirection = GetSampleDirection();\n\
-          vec3 N = normalize(sampleDirection);\n\
-          vec3 V = N;\n\
+					vec3 N = normalize(sampleDirection);\n\
+					vec3 V = N;\n\
 					mat3 vecSpace = MatrixFromVector(N);\n\
 					vec3 rgbColor = vec3(0.0);\n\
 					const int NumSamples = SAMPLES_PER_LEVEL;\n\
-          float solidAngleOfTexel = 4.0 * PI / (6.0 * sourceSize * sourceSize);\n\
+					float solidAngleOfTexel = 4.0 * PI / (6.0 * envMapFaceSize * envMapFaceSize);\n\
 					float weight = 0.0;\n\
 					for( int i = 0; i < NumSamples; i ++ ) {\n\
 						vec2 Xi = Hammersley2d(i, NumSamples);\n\
 						vec3 H = ImportanceSampleGGX(Xi, vecSpace);\n\
-            vec3 L = -reflect(V, H);\n\
+						vec3 L = -reflect(V, H);\n\
 						float NdotL = saturate(dot(N, L));\n\
 						float NdotH = saturate(dot(N, H));\n\
 						float HdotV = saturate(dot(H, V));\n\
-            if (NdotL > 0.0) {\n\
-              float D = DistributionGGX(NdotH);\n\
-              float pdf = (D * NdotH / (4.0 * HdotV));\n\
-              float solidAngleOfSample = 1.0 / (float(SAMPLES_PER_LEVEL) * pdf);\n\
-              float mipLevel = roughness == 0.0 ? 0.0 : 0.5 * log2(solidAngleOfSample / solidAngleOfTexel);\n\
-              rgbColor.rgb += envMapTexelToLinear(textureCube(envMap, L, mipLevel)).rgb * NdotL;\n\
-              weight += NdotL;\n\
-            }\n\
+						if (NdotL > 0.0) {\n\
+							float D = DistributionGGX(NdotH);\n\
+							float pdf = (D * NdotH / (4.0 * HdotV));\n\
+							float solidAngleOfSample = 1.0 / (float(SAMPLES_PER_LEVEL) * pdf);\n\
+							float mipLevel = roughness == 0.0 ? 0.0 : 0.5 * log2(solidAngleOfSample / solidAngleOfTexel);\n\
+							rgbColor.rgb += envMapTexelToLinear(textureCube(envMap, L, mipLevel)).rgb * NdotL;\n\
+							weight += NdotL;\n\
+						}\n\
 					}\n\
-          if (weight > 0.0) {\n\
-					  rgbColor /= weight;\n\
-          }\n\
+					if (weight > 0.0) {\n\
+						rgbColor /= weight;\n\
+					}\n\
 					//rgbColor = testColorMap();\n\
 					gl_FragColor = linearToOutputTexel( vec4( rgbColor, 1.0 ) );\n\
 				}",
